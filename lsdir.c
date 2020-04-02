2d61aca4e7bdabb0099cd0b734fa197823bfc3f3
mrboushy <mrboushy@users.noreply.github.com>
2020-04-02 12:55:52 -0700

    kinda works

--- lsdir.c
+++ lsdir.c
@@ -4,67 +4,61 @@
 #include <string.h>
 #include <unistd.h>
 
-// void printFolders(struct dirent *dir, DIR *dp);
-// void openfolders(char *fileName)
-// {
-//     DIR *dp;
-//     if ((dp = opendir(fileName)) == NULL || strcmp(fileName, "..") == 0)
-//     {
-//         /* code */
-//         //printf("error openning folder %s\n", fileName);
-//     }
-//     else
-//     {
-//         printFolders(readdir(dp), dp);
-//     }
-// }
-// void printFolders(struct dirent *dir, DIR *dp)
-// {
-//     if ((dir) != NULL)
-//     {
-//         // Skip removed files
-//         if (dir->d_ino == 0)
-//         {
-//         }
-//         else
-//         {
-//             char *fileName = dir->d_name;
-//             printf("%s\n", fileName);
-//             printFolders(readdir(dp), dp);
-//             if (strcmp(fileName, "..") != 0)
-//             {
-//                 chdir(fileName);
-//                 openfolders(fileName);
-//             }
-//         }
-//     }
-// }
+void openfolders(char *fileName)
+{
+    chdir(fileName);
+    DIR *dp;
+    struct dirent *dir;
+
+    if ((dp = opendir(".")) == NULL)
+    {
+        printf("error openning folder %s\n", fileName);
+    }
+
+    while ((dir = readdir(dp)) != NULL)
+    {
+        char *fileName = dir->d_name;
+        if (dir->d_ino == 0 || strcmp(fileName,".") == 0 || strcmp(fileName, "..") == 0)
+        {
+            continue;
+        }
+
+        if (dir->d_type & DT_DIR)
+        {
+            
+            printf("%s\n", fileName);
+
+            openfolders(fileName);
+            chdir("..");
+        }
+    }
+
+}
+
+
 int main(int argc, char *argv[])
 {
 
-    DIR *dp;
-    struct dirent *dir;
+    char *fileName;
+
+    if (argc < 2)
+    {
+        /* code */
+        fileName=".";
+    }else if(argc ==2)
+    {
+        fileName=argv[1];
+    }else
+    {
+        printf("lsdir: too many files.\n");
+        exit(1);
+    }
     
-    // if ((dp = opendir(".")) == NULL)
-    // {
-    //     fprintf(stderr, "cannot open directory.\n");
-    //     exit(1);
-    // }
+    
+    
 
-    // // Read entries
-    // while ((dir = readdir(dp)) != NULL)
-    // {
-    //     // Skip removed files
-    //     if (dir->d_ino == 0)
-    //     {
-    //         continue;
-    //     }
+    openfolders(fileName);
 
-    //     printf("%s\n", dir->d_name);
-    // }
-    //openfolders(".");
-  
-    closedir(dp);
 
     return 0;
 }
